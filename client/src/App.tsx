3e264afafdfedcebf1116f1300fd8a3ff7e3bfc2
Alfredo Chavez <alfredocharom@gmail.com>
2025-11-08 12:32:26 +0100

    Add comments about routing to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import SparkComponent from './components/SparkComponent';
 import SplashScreen from './components/SplashScreen';
 import { Routes, Route } from 'react-router';
 
+//? When navigating to a new route, you can see all the new components getting mounted for a split second. How can we avoid this?
 function App() {
 
   return (
